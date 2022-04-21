19193ab0be839fdd2d269f4ebf8ac8854e73a5a1
iraisav <zs19004913@estudiantes.uv.mx>
2022-04-20 23:03:17 -0500

    configurando storage y firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD5UVjzIhAgoXKCR2EinoqlI0dmJrYo7Rs",
+    authDomain: "fakestagram-597e1.firebaseapp.com",
+    projectId: "fakestagram-597e1",
+    storageBucket: "fakestagram-597e1.appspot.com",
+    messagingSenderId: "4792777977",
+    appId: "1:4792777977:web:15ef58dbe0910977c97628"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
