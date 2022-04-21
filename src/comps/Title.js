c826dd3252f5bfc849e7d0beca408afd115ce1d4
maldonadojudith18 <judith1821maldonado@gmail.com>
2022-04-20 22:35:14 -0500

    creando modelo

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h1>Fakestagram</h1>
+      <h2>TU GALERIA</h2>
+      <p>Sube tus imagenes favoritas en fakestagram</p>
     </div>
   )
 }
